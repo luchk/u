07b0fcfc60fa26b71a1fcc3fbba0d22de3f473c5
luchk <30504944+luchk@users.noreply.github.com>
2017-10-31 20:39:20 +0200

    1

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hsadasdadadsaassasdasdasadsello, World!\n");
+    printf("Hsadasdadadsaassasdasdaвив ви в и ви    ивв и     вв  ви в  ив и  ивsadsello, World!\n");
     return 0;
 }
